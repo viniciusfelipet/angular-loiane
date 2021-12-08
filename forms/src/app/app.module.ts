313751f7d4559ffdd758877133438eeee0c9b2fd
Vinicius-Felipe-T <vinicius.felipe159@gmail.com>
2021-12-07 22:17:04 -0300

    forms até #80

---
+++ forms/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DataFormComponent } from './data-form/data-form.component';
+import { TemplateFormModule } from './template-form/template-form.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataFormComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    TemplateFormModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ forms/src/app/template-form/template-form.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-form',
+  templateUrl: './template-form.component.html',
+  styleUrls: ['./template-form.component.css']
+})
+export class TemplateFormComponent implements OnInit {
+
+  usuario: any = {
+    nome: null,
+    email: null
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(form) {
+    console.log(form);
+    console.log(this.usuario);
+    
+  }
+
+}
